e0ccc7755a32ece46811270872f032af3ffd551b
guanyj <767475042@qq.com>
2017-04-17 20:04:12 +0800

    博客列表--分页

---
+++ public/control/viewModule/pager.module.js
@@ -0,0 +1,190 @@
+/**
+ * Created by Administrator on 2017/4/17 0017.
+ */
+define(['jquery','../data/pager.data'],function ($,$data) {
+    var c,t;
+    var r;
+    /**
+     * 分页样式控制函数
+     * @param c {当前页}
+     * @param t {总页数}
+     */
+    function _pagation(currentNum,totalNum,request) {
+        c = parseInt(currentNum);
+        t = parseInt(totalNum);
+        if(request){
+            r = request;
+        }
+        var $num = $(".page-num"),
+            $dot_prev = $(".dot-prev"),
+            $dot_next = $(".dot-next");
+        $num.removeClass('page-on');
+        if(t < 5){
+            $dot_prev.hide();
+            $dot_next.hide();
+            $.each($num,function (index,item) {
+                if(index < t){
+                    if(index + 1 === c){
+                        $(item).addClass('page-on');
+                    }
+                    $(item).html(index+1).show();
+                }
+                else{
+                    $(item).html('').hide();
+                }
+            });
+        }
+        else if(t > 9){
+            $dot_prev.show();
+            $dot_next.show();
+            if(c < 5){
+                $dot_prev.hide();
+                $.each($num,function (index,item) {
+                    if(index <= c){
+                        $(item).html(index + 1).show();
+                        if(index === c -1){
+                            $(item).addClass('page-on');
+                        }
+                    }
+                    else if(index >= 5){
+                        $(item).html(t - 6 + index).show();
+                    }
+                    else{
+                        $(item).html('').hide();
+                    }
+                });
+            }
+            else if(c > t - 4){
+                $dot_next.hide();
+                var currentIndex = t - c;
+                $.each($num,function (index,item) {
+                    if(index >= 6 - currentIndex - 1){
+                        $(item).html(t - (6 - index)).show();
+                        if(6 - index === currentIndex){
+                            $(item).addClass('page-on');
+                        }
+                    }
+                    else if(index <= 1){
+                        $(item).html(index + 1).show();
+                    }
+                    else{
+                        $(item).html('').hide();
+                    }
+                });
+            }
+            else{
+                $.each($num,function (index,item) {
+                    if(index <= 1){
+                        $(item).html(index + 1).show();
+                    }
+                    else if(index >= 5){
+                        $(item).html(t - (6 - index)).show();
+                    }
+                    else{
+                        $(item).html(c - 3 + index).show();
+                        if(index === 3){
+                            $(item).addClass('page-on');
+                        }
+                    }
+                });
+            }
+        }
+        else{
+            if(c >= 5){
+                $dot_prev.show();
+                $dot_next.hide();
+                $.each($num,function (index,item) {
+                    if(index <= 1){
+                        $(item).html(index + 1).show();
+                    }
+                    else if(index + 1 >= 6 - (t - c)){
+                        if(index === 6 - (t - c)){
+                            $(item).addClass('page-on');
+                        }
+                        $(item).html(t - (6 - index)).show();
+                    }
+                    else{
+                        $(item).html('').hide();
+                    }
+                });
+            }
+            else if(c <= t - 4){
+                $dot_prev.hide();
+                $dot_next.show();
+                $.each($num,function (index,item) {
+                    if(index <= c){
+                        if(index+1 === c){
+                            $(item).addClass('page-on');
+                        }
+                        $(item).html(index+1).show();
+                    }
+                    else if(index >= 5){
+                        $(item).html(t - (6 - index)).show();
+                    }
+                    else{
+                        $(item).html('').hide();
+                    }
+                });
+            }
+            else{
+                $dot_prev.hide();
+                $dot_next.hide();
+                $.each($num,function (index,item) {
+                    if(index + 1 === c){
+                        $(item).addClass('page-on');
+                    }
+                    if(index < t){
+                        $(item).html(index+1).show();
+                    }
+                    else{
+                        $(item).html('').hide();
+                    }
+                });
+            }
+        }
+    }
+    /**
+     * 上一页事件
+     */
+    function page_prev() {
+        if(c === 1){
+            alert("已经是第一页了~")
+        }
+        else{
+            _pagation(--c,t);
+        }
+    }
+    /**
+     * 下一页事件
+     */
+    function page_next() {
+        if(c === t){
+            alert("已经是第一页了~")
+        }
+        else{
+            _pagation(++c,t);
+        }
+    }
+
+    /**
+     * 页码点击事件
+     * @param num
+     */
+    function page_num(num) {
+        num = parseInt($(num).text());
+        if(num !== c){
+            _pagation(num,t);
+        }
+    }
+    return {
+        pagation:_pagation,
+        page_prev:page_prev,
+        page_next:page_next,
+        page_num:page_num
+    }
+});
+
+
+
+
+
